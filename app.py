84db35177067490ed0e24e0357c372310aa120db
Poomon <poomon2542@gmail.com>
2020-09-04 06:49:11 -0700

    test

--- app.py
+++ app.py
@@ -14,13 +14,42 @@ def home():
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     if request.method == "POST":
+        target = os.path.join(APP_ROOT)
 
-        return render_template("upload.html", text='filename' + " is saved")
+        ''' get file from the submitted form '''
+        file = request.files.get("file")
+
+        ''' get file name '''
+        filename = file.filename
+
+        ''' path + file name = saving location '''
+        destination = "./".join([target, filename])
+        file.save(destination)
+        make()
+        return render_template("upload.html", text=filename + " is saved")
     else:
         return render_template("upload.html", text="Upload docx file")
 
 def make():
     print("hello")
+    model = ""
+    doc = docx.Document("model.docx")
+    file = doc.paragraphs
+
+    ''' create style obj (access by document.styles) '''
+    style = doc.styles['Normal']
+
+    ''' modify font (style.class.method) '''
+    font = style.font
+    font.name = 'Arial'
+    font.size = docx.shared.Pt(18)
+
+    for line in (file):
+        print(line.text)
+        model += line.text
+        model += ("\n")
+
+    doc.save("output.docx")
 
 if (__name__) == "__main__":
     app.run(debug=True)
